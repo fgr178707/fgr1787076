97795ed89f75cef866254e94b1f5fbf650688c4d
fgr178707 <fgr178707@163.com>
2023-12-26 16:26:17 +0800

    网站地址

--- js/Tampermonkey/关闭 Minecraft 官网弹出提示跳转到网易.js
+++ js/Tampermonkey/关闭 Minecraft 官网弹出提示跳转到网易.js
@@ -7,6 +7,8 @@
 // @match        https://www.minecraft.net/*
 // @icon         https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/favicon-32x32.png
 // @grant        none
+// @downloadURL https://update.greasyfork.org/scripts/452297/%E5%85%B3%E9%97%AD%20Minecraft%20%E5%AE%98%E7%BD%91%E5%BC%B9%E5%87%BA%E6%8F%90%E7%A4%BA.user.js
+// @updateURL https://update.greasyfork.org/scripts/452297/%E5%85%B3%E9%97%AD%20Minecraft%20%E5%AE%98%E7%BD%91%E5%BC%B9%E5%87%BA%E6%8F%90%E7%A4%BA.meta.js
 // ==/UserScript==
 
 (function () {
